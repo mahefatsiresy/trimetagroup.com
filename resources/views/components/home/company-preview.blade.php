06d60c6e56dcaa8a337d345d4e32e1949d8d5b9c
mahefatsiresy <mahefatsiresy.n.lucas@gmail.com>
2023-10-02 09:18:27 +0300

    fix company preview link

--- resources/views/components/home/company-preview.blade.php
+++ resources/views/components/home/company-preview.blade.php
@@ -12,7 +12,7 @@
     class="w-full h-[512px] lg:w-[calc(100vw/6)] lg:h-full bg-center bg-cover bg-no-repeat hover:w-[456px] transition-all ease-in-out duration-1000 cursor-pointer delay-0"
     style="background-image: url({{ $background }});z-index: {{ $index + 2 }};">
 
-    <a href="/activities/{{ $href }}`"
+    <a href="/activities/{{ $href }}"
         class="bg-gradient-to-t from-gray-950/90 from-35% w-full h-full flex flex-col justify-end p-4 transition-none">
 
         <p class="mb-4 w-[164px]">{{ $description }}</p>
