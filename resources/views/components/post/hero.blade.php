b5b894ebe0d502eba1d89ca7073cb32e8ba29afe
mahefatsiresy <mahefatsiresy.n.lucas@gmail.com>
2023-11-25 20:33:21 +0300

    udpate: post hero and preview ui

--- resources/views/components/post/hero.blade.php
+++ resources/views/components/post/hero.blade.php
@@ -2,23 +2,23 @@
 <x-bg-with-motion :background="$post['thumbnail']">
     <header class="h-[90vh] lg:min-h-[464px] md:h-[80vh] max-h-[640px] relative w-full">
         @if (!$post['thumbnail'])
-            <div class="h-full w-full -z-10 absolute inset-0 bg-gray-800 text-gray-500 grid place-items-center">
+            <div class="h-full w-full -z-10 absolute inset-0 bg-neutral-800 text-neutral-500 grid place-items-center">
                 No featured image
             </div>
         @endif
         <div
-            class="h-3/4 md:h-1/2 w-full absolute bottom-0 bg-gradient-to-t from-15% lg:from-45% from-gray-950/70 flex flex-col justify-end md:flex-row  md:items-end md:justify-between pb-4 px-4 md:px-12  text-gray-100">
+            class="h-3/4 md:h-1/2 w-full absolute bottom-0 bg-gradient-to-t from-15% lg:from-45% from-neutral-950/70 flex flex-col justify-end md:flex-row  md:items-end md:justify-between pb-4 px-4 md:px-12  text-neutral-100">
             <div class="flex flex-col">
-                <h1 class="text-2xl lg:text-4xl font-bold mb-4 w-fit max-w-[1200px] font-[Poppins_Inter]">
+                <h1 class="text-3xl lg:text-5xl font-semibold mb-4 w-fit max-w-[1200px]" style="font-family: Poppins;">
                     {{ $post['title'] }}
                 </h1>
-                <div class="text-sm w-fit text-gray-400">
+                <div class="text-sm w-fit text-neutral-400">
                     {{ $post['date'] }}
                 </div>
             </div>
 
             @if ($withCta)
-                <x-button href="/news{{ $post['slug'] }}">{{ __('common.news.post.cta') }}</x-button>
+                <x-button class="self-end" href="/news/{{ $post['slug'] }}">{{ __('common.news.post.cta') }}</x-button>
             @endif
 
         </div>
--- resources/views/components/post/preview.blade.php
+++ resources/views/components/post/preview.blade.php
@@ -1,31 +1,31 @@
 @props(['post', 'dark' => false])
 
-<li class="rounded-md grid gap-4 lg:gap-8 lg:grid-cols-[400px_400px] md:grid-cols-[352px_auto] lg:w-fit">
-    <a href="/news/{{ $post['slug'] }}" class="overflow-hidden w-full block rounded-md">
+<li>
+    <a href="/news/{{ $post['slug'] }}" class="overflow-hidden w-full block">
         @if ($post['thumbnail'])
             <img src="{{ $post['thumbnail'] }}" alt="thubmnail of {{ $post['slug'] }}" width="800" height="800"
-                class="w-full h-[224px] object-cover hover:scale-105 duration-300" />
+                class="w-full h-[256px] object-cover hover:scale-105 duration-300" />
         @else
-            <div class="w-full h-[224px] bg-gray-900 text-gray-600 flex items-center justify-center">
+            <div class="w-full h-[256px] bg-neutral-800 text-neutral-600 flex items-center justify-center">
                 {{ __('common.news.post.no-image') }}
             </div>
         @endif
     </a>
-    <div class="flex flex-col justify-between gap-3">
+    <div class="flex flex-col justify-between gap-3 bg-gradient-to-br from-neutral-950 to bg-neutral-900 p-6">
         <div>
-            <h3 class="{{ 'text-xl lg:text-2xl font-semibold mb-2 ' . ($dark ? 'text-gray-100' : 'text-gray-900') }}">
-                <a class="hover:text-green-700" href="/news/{{ $post['slug'] }}">
+            <h3 class="text-xl lg:text-2xl font-semibold mb-2 text-neutral-100">
+                <a href="/news/{{ $post['slug'] }}">
                     {{ $post['title'] }}
                 </a>
             </h3>
 
-            <div class="text-gray-500 text-sm font-semibold">{{ $post['date'] }}</div>
+            <div class="text-neutral-500 text-sm font-semibold">{{ $post['date'] }}</div>
 
-            <div class="{{ 'line-clamp-3 mt-4' . ($dark ? 'text-gray-700' : 'text-gray-400') }}">
+            <div class="line-clamp-3 mt-4 text-neutral-400">
                 {!! $post['excerpt'] !!}
             </div>
         </div>
-        <x-button href="/news/{{ $post['slug'] }}" class="self-end lg:self-start"
+        <x-button href="/news/{{ $post['slug'] }}" class="mt-8 w-3/4 text-center mx-auto"
             secondary>{{ __('common.news.post.cta') }}</x-button>
     </div>
 </li>
