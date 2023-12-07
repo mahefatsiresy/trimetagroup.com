9568dd42a061b76c3b49b7a58fe438791b990cbc
mahefatsiresy <mahefatsiresy.n.lucas@gmail.com>
2023-12-07 12:45:06 +0300

    update: translation data structure

--- lang/en/trimeta-agrofood.php
+++ lang/en/trimeta-agrofood.php
@@ -19,15 +19,30 @@ return [
 
     // 'details.three.icon' => '/icons/certifications.png',
     // 'details.three.title' => 'Certifications',
-    'certifications.descriptions.one' => 'FAIRTRADE by FLOCERT',
-    'certifications.descriptions.two' => 'BIO (EOS/NOP) by ECOCERT',
-    'certifications.descriptions.three' => 'UEBT - RA by ECOCERT/UEBT',
 
-    'certifications.images.one' => '/images/logo/agriculture_biologique.png',
-    'certifications.images.two' => '/images/logo/mg-bio-154.png',
-    'certifications.images.three' => '/images/logo/usda.png',
-    'certifications.images.four' => '/images/logo/fairtrade.png',
-    'certifications.images.five' => '/images/logo/rainforest-alliance.png',
+    // 'certifications.descriptions.one' => 'FAIRTRADE by FLOCERT',
+    // 'certifications.descriptions.two' => 'BIO (EOS/NOP) by ECOCERT',
+    // 'certifications.descriptions.three' => 'UEBT - RA by ECOCERT/UEBT',
+
+    // 'certifications.images.one' => '/images/logo/agriculture_biologique.png',
+    // 'certifications.images.two' => '/images/logo/mg-bio-154.png',
+    // 'certifications.images.three' => '/images/logo/usda.png',
+    // 'certifications.images.three.desc' => '...',
+    // 'certifications.images.four' => '/images/logo/fairtrade.png',
+    // 'certifications.images.four.desc' => 'Norme de production durable de transformation res...',
+    // 'certifications.images.five' => '/images/logo/rainforest-alliance.png',
+
+    'certifications.one.title' => 'BIO (EOS/NOP) by ECOCERT',
+    'certifications.one.images' => '/images/logo/usda.png',
+    'certifications.one.descriptions' => '<p>ORGANIC EOS-USDA NOP and equivalence NOP-COR is a production, processing and handling standard for pesticide-free products for consumers.</p><p>Why get certified? To guarantee healthy, pesticide-free products for consumers and environmentally-friendly production for producers.</p><p> How Trimeta Agrofood became certified:<br>Close, long-term collaboration with producer groups and specific farms. Training, assistance and support for producers throughout the production process.</p>',
+
+    'certifications.two.title' => 'UEBT-RA by ECOCERT/UEBT',
+    'certifications.two.images' => '/images/logo/ubet-ra.jpeg',
+    'certifications.two.descriptions' => '<p>UEBT-RA: is the fusion of two standards UEBT and Rainforest Alliance, promotes ethical and sustainable sourcing; a system has been put in place throughout sourcing to comply with UEBT/RA standard requirements. The aim: to assure consumers of our commitment to biodiversity sourcing practices that aim to regenerate ecosystems and with social respect.</p><p>Why get certified? For sustainable production and respect for the social value of people in the supply chain, as well as business ethics.</p><p>UEBT/RA certification is issued by ECOCERT, an organization accredited by UEBT, and compliance with the UEBT/RA standard is an effective way of demonstrating our commitment to sustainable development.  Consumers are reassured that ingredients are sourced with respect for people and biodiversity.</p><p>How Trimeta Agrofood achieved certification:<br>Close, long-term collaboration with producer groups committed to meeting UEBT/RA requirements on specific farms. Training, assistance and support and CSR at producer level throughout production.</p>',
+
+    'certifications.three.title' => 'FAIRTRADE by FLOCERT',
+    'certifications.three.images' => '/images/logo/fairtrade.png',
+    'certifications.three.descriptions' => '<p>Fairtrade: a standard for sustainable production, processing that respects the social value of human beings and fair trade.</p><p>Why get certified? For sustainable production and respect for the social value of man in production, as well as fair trade ethics.</p><p>How Trimeta Agrofood became certified:<br>Close, long-term collaboration with producer groups and specific farms. Training, assistance and CSR support for producers throughout the production process.</p>',
 
 
     'contacts.phone' => 'phone',
